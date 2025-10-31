fbd35e565c35c79dbf3e4a35eadcdd2dd9b2f551
Mitch <mitch.hare03@gmail.com>
2025-10-31 21:57:26 +1100

    Restrict host permissions to udio.com

--- background.js
+++ background.js
@@ -1,6 +1,13 @@
 chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ["content.js"]
-  });
+  if (tab.url && tab.url.includes("udio.com")) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ["content.js"]
+    });
+  } else {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: () => alert("This extension only works on udio.com")
+    });
+  }
 });
